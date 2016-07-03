a229dd12955489a79fde2ced1d403117520d17c0
overlookmotel <theoverlookmotel@gmail.com>
2016-07-03 03:06:14 +0100

    Tests WIP 12

--- test/methods/catch.test.js
+++ test/methods/catch.test.js
@@ -1,29 +1,35 @@
 /*
  * cls-bluebird tests
- * Tests for .catch()
+ * Tests for .catch() / .error()
  */
 
-/* global describe */
+/* global it */
+
+// Modules
+var expect = require('chai').expect;
 
 // Imports
 var runTests = require('../support');
 
 // Run tests
 
-runTests('.catch()', function(Promise, u) { // jshint ignore:line
-    describe('returns instance of patched Promise constructor when handler', function() {
-        u.testSetProtoMethodReturnsPromise(function(p, handler) {
-            return p.catch(handler);
-        }, {catches: true});
-    });
-
-    describe('calls callback asynchronously when handler', function() {
-        u.testSetCallbackAsync(function(p, handler) {
-            p.catch(handler);
-        }, {catches: true});
-    });
-
-    u.testSetCallbackBound(function(p, handler) {
-        p.catch(handler);
+runTests('.catch()', function(u) {
+    u.testSetProtoMethodAsync(function(p, handler) {
+        return p.catch(handler);
     }, {catches: true});
 });
+
+runTests('.caught()', function(u, Promise) { // jshint ignore:line
+    it('is alias of .catch()', function() {
+        expect(Promise.prototype.caught).to.equal(Promise.prototype.catch);
+    });
+});
+
+/*
+// TODO make this work - .error() only catches operational errors
+runTests('.error()', function(u) {
+    u.testSetProtoMethodAsync(function(p, handler) {
+        return p.error(handler);
+    }, {catches: true});
+});
+*/
