8f3faa2b34b9d2d2b50e396d2e1134608a2202c0
overlookmotel <theoverlookmotel@gmail.com>
2016-09-02 02:56:14 +0100

    Tests for `new Promise` resolving with promise

--- test/methods/core/constructor.test.js
+++ test/methods/core/constructor.test.js
@@ -12,44 +12,37 @@ var runTests = require('../../support');
 
 runTests('new Promise()', function(u, Promise) {
 	describe('returns instance of patched Promise constructor when', function() {
-		u.describeMainPromises(function(makePromise) {
-			u.testIsPromise(function(cb) {
-				var p = makePromise();
-				cb(p);
-			});
-		});
-
-		/*
-		// TODO Add tests for when resolved with promise e.g. `new Promise(function(resolve) { resolve(Promise.resolve(); )})`
-		describe('resolves with', function() {
-			testValues(function(value) {
-				return new Promise(function(resolve) {
-					resolve(value);
+		describe('resolves', function() {
+			u.describeAttachSimple(function(attach) {
+				describe('with', function() {
+					u.describeValues(function(makeValue) {
+						u.testIsPromise(function(cb) {
+							var p = new Promise(function(resolve) {
+								attach(function() {
+									resolve(makeValue());
+								});
+							});
+							u.inheritRejectStatus(p, makeValue);
+							cb(p);
+						});
+					});
 				});
 			});
 		});
 
-		describe('rejects with', function() {
-			testValues(function(value) {
-				var p = new Promise(function(resolve, reject) { // jshint ignore:line
-					reject(value);
-				});
-				u.setRejectStatus(p);
-				return p;
-			});
-		});
-
-		function testValues(fn) {
-			u.describeValues(function(makeValue) {
+		describe('rejects', function() {
+			u.describeAttachSimple(function(attach) {
 				u.testIsPromise(function(cb) {
-					var value = makeValue();
-					var p = fn(value);
-					u.inheritRejectStatus(p, value);
+					var p = new Promise(function(resolve, reject) { // jshint ignore:line
+						attach(function() {
+							reject(u.makeError());
+						});
+					});
+					u.setRejectStatus(p);
 					cb(p);
 				});
 			});
-		}
-		*/
+		});
 
 		it('unresolved', function(done) {
 			var p = new Promise(function() {});
